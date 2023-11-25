6574ca885c7e4051e4cad552f6140099003fee4c
coquizin <cauli.vilela@live.com>
2023-11-24 21:04:33 -0300

    some apis example

--- src/modules/newsletter/screens/Noticias/index.tsx
+++ src/modules/newsletter/screens/Noticias/index.tsx
@@ -2,8 +2,18 @@ import Image from "next/image";
 import Link from "next/link";
 import { motion } from "framer-motion";
 import HeroPage from "@/components/heroPages";
+import {
+  useGetNoticiasSpaceX,
+  useGetShips,
+} from "../../../../services/noticias";
+import { formatToDate } from "brazilian-values";
+import { useEffect } from "react";
 
 export default function NewsLetterScreen() {
+  const { data, isLoading } = useGetShips();
+
+  console.log(data);
+
   return (
     <>
       <HeroPage
@@ -13,32 +23,46 @@ export default function NewsLetterScreen() {
       <div className=" bg-black min-h-screen w-full pb-20 relative px-12 pt-[90px]">
         <div className="flex flex-col items-center z-[2] w-full gap-10 ">
           <div className="flex flex-wrap justify-start w-full max-w-[1680px] gap-5">
-            {Array(5)
-              .fill(0)
-              .map((_, index) => (
+            {!isLoading &&
+              data &&
+              data.map((noticia) => (
                 <div
-                  key={index}
+                  key={noticia.ship_id}
                   className=" mb-24 min-h-[280px]   relative  text-[#fafafa] w-full md:basis-project-card-half"
                 >
                   <div className="relative cursor-pointer p-5 z-[1] pb-[65%]">
                     <Link href={"/noticias/loren-ipson"}>
-                      <Image
-                        src={"/assets/images/noiterocket.jpg"}
-                        alt={"illustriation1_d"}
-                        width={1920}
-                        loading="lazy"
-                        onLoad={(e) => {
-                          e.currentTarget.classList.add("animate-fade-in");
-                        }}
-                        height={1080}
-                        className="absolute top-0 opacity-0 left-0 object-cover object-center max-h-full h-full min-h-full z-[1] "
-                      />
+                      {noticia.image ? (
+                        <Image
+                          src={noticia?.image}
+                          alt={"illustriation1_d"}
+                          width={1920}
+                          loading="lazy"
+                          onLoad={(e) => {
+                            e.currentTarget.classList.add("animate-fade-in");
+                          }}
+                          height={1080}
+                          className="absolute top-0 opacity-0 left-0 object-cover object-center max-h-full h-full min-h-full z-[1] "
+                        />
+                      ) : (
+                        <Image
+                          src={"/assets/images/noiterocket.jpg"}
+                          alt={"illustriation1_d"}
+                          width={1920}
+                          loading="lazy"
+                          onLoad={(e) => {
+                            e.currentTarget.classList.add("animate-fade-in");
+                          }}
+                          height={1080}
+                          className="absolute top-0 opacity-0 left-0 object-cover object-center max-h-full h-full min-h-full z-[1] "
+                        />
+                      )}
                     </Link>
                   </div>
                   <div className="relative mt-12 z-[2]">
-                    <p className="text-xl opacity-80">20 de Novembro de 2023</p>
+                    <p className="text-xl opacity-80">{noticia.home_port}</p>
                     <p className="mt-3 text-xl uppercase font-korataki">
-                      Projeto STRAUM
+                      {noticia.ship_name}
                     </p>
                     <motion.div
                       initial={{ opacity: 0, y: 100 }}
